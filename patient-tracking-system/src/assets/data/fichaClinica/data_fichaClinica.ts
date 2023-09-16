5c2955e4635791c6aa72a7b8224ad419953196bb
Emilio Ginzo <emilioginzo@fpuna.edu.py>
2023-09-16 19:47:58 -0400

    dummy data for ficha clinica

---
+++ patient-tracking-system/src/assets/data/fichaClinica/data_fichaClinica.ts
@@ -0,0 +1,187 @@
+import { FichaClinica } from "src/app/components/ficha-clinica/ficha-clinica.interface"
+import { Categoria } from "src/app/components/consulta/consulta.interface"
+
+
+export const Data_fichasClinicas: FichaClinica[] = [
+    {
+      "id": 1,
+      "motivo": "Dolor de cabeza",
+      "diagnostico": "Migraña",
+      "categoria": 5
+    },
+    {
+      "id": 2,
+      "motivo": "Fiebre",
+      "diagnostico": "Resfriado común",
+      "categoria": 3
+    },
+    {
+      "id": 3,
+      "motivo": "Dolor abdominal",
+      "diagnostico": "Apendicitis",
+      "categoria": 8
+    },
+    {
+      "id": 4,
+      "motivo": "Fatiga",
+      "diagnostico": "Anemia",
+      "categoria": 6
+    },
+    {
+      "id": 5,
+      "motivo": "Dolor en el pecho",
+      "diagnostico": "Infarto de miocardio",
+      "categoria": 9
+    },
+    {
+      "id": 6,
+      "motivo": "Náuseas",
+      "diagnostico": "Gastritis",
+      "categoria": 4
+    },
+    {
+      "id": 7,
+      "motivo": "Problemas respiratorios",
+      "diagnostico": "Asma",
+      "categoria": 7
+    },
+    {
+      "id": 8,
+      "motivo": "Dolor de espalda",
+      "diagnostico": "Hernia de disco",
+      "categoria": 2
+    },
+    {
+      "id": 9,
+      "motivo": "Dolor de garganta",
+      "diagnostico": "Amigdalitis",
+      "categoria": 1
+    },
+    {
+      "id": 10,
+      "motivo": "Mareos",
+      "diagnostico": "Vértigo",
+      "categoria": 10
+    },
+    {
+        "id": 11,
+        "motivo": "Dolor de cabeza",
+        "diagnostico": "Migraña",
+        "categoria": 5
+      },
+      {
+        "id": 12,
+        "motivo": "Fiebre",
+        "diagnostico": "Resfriado común",
+        "categoria": 3
+      },
+      {
+        "id": 13,
+        "motivo": "Dolor abdominal",
+        "diagnostico": "Apendicitis",
+        "categoria": 8
+      },
+      {
+        "id": 14,
+        "motivo": "Fatiga",
+        "diagnostico": "Anemia",
+        "categoria": 6
+      },
+      {
+        "id": 15,
+        "motivo": "Dolor en el pecho",
+        "diagnostico": "Infarto de miocardio",
+        "categoria": 9
+      },
+      {
+        "id": 16,
+        "motivo": "Náuseas",
+        "diagnostico": "Gastritis",
+        "categoria": 4
+      },
+      {
+        "id": 17,
+        "motivo": "Problemas respiratorios",
+        "diagnostico": "Asma",
+        "categoria": 7
+      },
+      {
+        "id": 18,
+        "motivo": "Dolor de espalda",
+        "diagnostico": "Hernia de disco",
+        "categoria": 2
+      },
+      {
+        "id": 19,
+        "motivo": "Dolor de garganta",
+        "diagnostico": "Amigdalitis",
+        "categoria": 1
+      },
+      {
+        "id": 20,
+        "motivo": "Mareos",
+        "diagnostico": "Vértigo",
+        "categoria": 10
+      },
+      {
+        "id": 21,
+        "motivo": "Dolor de cabeza",
+        "diagnostico": "Migraña",
+        "categoria": 5
+      },
+      {
+        "id": 22,
+        "motivo": "Fiebre",
+        "diagnostico": "Resfriado común",
+        "categoria": 3
+      },
+      {
+        "id": 23,
+        "motivo": "Dolor abdominal",
+        "diagnostico": "Apendicitis",
+        "categoria": 8
+      },
+      {
+        "id": 24,
+        "motivo": "Fatiga",
+        "diagnostico": "Anemia",
+        "categoria": 6
+      },
+      {
+        "id": 25,
+        "motivo": "Dolor en el pecho",
+        "diagnostico": "Infarto de miocardio",
+        "categoria": 9
+      },
+      {
+        "id": 26,
+        "motivo": "Náuseas",
+        "diagnostico": "Gastritis",
+        "categoria": 4
+      },
+      {
+        "id": 27,
+        "motivo": "Problemas respiratorios",
+        "diagnostico": "Asma",
+        "categoria": 7
+      },
+      {
+        "id": 28,
+        "motivo": "Dolor de espalda",
+        "diagnostico": "Hernia de disco",
+        "categoria": 2
+      },
+      {
+        "id": 29,
+        "motivo": "Dolor de garganta",
+        "diagnostico": "Amigdalitis",
+        "categoria": 1
+      },
+      {
+        "id": 30,
+        "motivo": "Mareos",
+        "diagnostico": "Vértigo",
+        "categoria": 10
+      }
+  ]
+  
