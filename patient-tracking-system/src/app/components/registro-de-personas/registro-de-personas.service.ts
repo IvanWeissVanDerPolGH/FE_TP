09d820beae19009bfc42db0993be2303fd9fa341
Emilio Ginzo <emilioginzo@fpuna.edu.py>
2023-09-16 03:54:25 -0400

    cambio el nombre de la variable

--- patient-tracking-system/src/app/components/registro-de-personas/registro-de-personas.service.ts
+++ patient-tracking-system/src/app/components/registro-de-personas/registro-de-personas.service.ts
@@ -7,18 +7,18 @@ import { Data_RegistroDePersonas } from '../../../assets/data/RegistroDePersonas
   providedIn: 'root'
 })
 export class RegistroPersonaService {
-  private listaPersonas: Persona[]
+  private personas: Persona[]
 
 
   constructor() {
-    this.listaPersonas = Data_RegistroDePersonas
+    this.personas = Data_RegistroDePersonas;
   }
 
   // Read operation: Get the list of personas
   getPersonas_sample(): Observable<Persona[]> {
     // You can return the list of personas directly or fetch from an API if needed
     return new Observable<Persona[]>((observer) => {
-      observer.next(this.listaPersonas);
+      observer.next(this.personas);
       observer.complete();
     });
   }
@@ -30,7 +30,7 @@ export class RegistroPersonaService {
     persona.idPersona = this.generateNewId();
 
     // Push the new persona to the data source
-    this.listaPersonas.push(persona);
+    this.personas.push(persona);
 
     // You can save the updated data source to a file or an API if needed
 
@@ -42,11 +42,11 @@ export class RegistroPersonaService {
 
   // Update operation: Update an existing persona
   updatePersona(persona: Persona): Observable<void> {
-    const index = this.listaPersonas.findIndex((p) => p.idPersona === persona.idPersona);
+    const index = this.personas.findIndex((p) => p.idPersona === persona.idPersona);
 
     if (index !== -1) {
       // Replace the existing persona with the updated one
-      this.listaPersonas[index] = persona;
+      this.personas[index] = persona;
 
       // You can save the updated data source to a file or an API if needed
     }
@@ -59,11 +59,11 @@ export class RegistroPersonaService {
 
   // Delete operation: Remove a persona by ID
   deletePersona(idPersona: number): Observable<void> {
-    const index = this.listaPersonas.findIndex((p) => p.idPersona === idPersona);
+    const index = this.personas.findIndex((p) => p.idPersona === idPersona);
 
     if (index !== -1) {
       // If the persona is found, remove it from the array
-      this.listaPersonas.splice(index, 1);
+      this.personas.splice(index, 1);
 
       // You can save the updated data source to a file or an API if needed
     }
@@ -77,7 +77,7 @@ export class RegistroPersonaService {
   // Generate a new unique ID (You may implement your own logic)
   private generateNewId(): number {
     // Find the maximum ID in the current data
-    const maxId = Math.max(...this.listaPersonas.map((p) => p.idPersona));
+    const maxId = Math.max(...this.personas.map((p) => p.idPersona));
 
     // Generate a new ID by incrementing the maximum ID
     return maxId + 1;
