a45538461c224176ed59175f4de5c44c102aa53a
MarcoAurelioAcosta <marcochongsu@fpuna.edu.py>
2023-09-17 16:38:22 -0400

    Update data_consulta.ts

--- patient-tracking-system/src/assets/data/consulta/data_consulta.ts
+++ patient-tracking-system/src/assets/data/consulta/data_consulta.ts
@@ -1,14 +1,14 @@
 import { Categoria } from "src/app/components/consulta/consulta.interface"
 
-export const Data_consulta: Categoria[] =   [
-    { "id": 1, "descripcion": "EJEMPLO 1", isEditing: false },
-    { "id": 2, "descripcion": "EJEMPLO 2", isEditing: false },
-    { "id": 3, "descripcion": "EJEMPLO 3", isEditing: false },
-    { "id": 4, "descripcion": "EJEMPLO 4", isEditing: false },
-    { "id": 5, "descripcion": "EJEMPLO 5", isEditing: false },
-    { "id": 6, "descripcion": "EJEMPLO 6", isEditing: false },
-    { "id": 7, "descripcion": "EJEMPLO 7", isEditing: false },
-    { "id": 8, "descripcion": "EJEMPLO 8", isEditing: false },
-    { "id": 9, "descripcion": "EJEMPLO 9", isEditing: false },
-    { "id": 10, "descripcion": "EJEMPLO 10", isEditing: false }
-]
+export const Data_consulta: Categoria[] = [
+    { "id": 1, "descripcion": "Cardiología", isEditing: false },
+    { "id": 2, "descripcion": "Dermatología", isEditing: false },
+    { "id": 3, "descripcion": "Gastroenterología", isEditing: false },
+    { "id": 4, "descripcion": "Neurología", isEditing: false },
+    { "id": 5, "descripcion": "Oftalmología", isEditing: false },
+    { "id": 6, "descripcion": "Ortopedia", isEditing: false },
+    { "id": 7, "descripcion": "Pediatría", isEditing: false },
+    { "id": 8, "descripcion": "Psiquiatría", isEditing: false },
+    { "id": 9, "descripcion": "Radiología", isEditing: false },
+    { "id": 10, "descripcion": "Urología", isEditing: false }
+];
